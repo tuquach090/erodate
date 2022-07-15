c412008b9a0c95610f6355e388bf19661b8c039d
Tu <tuquac090@gmail.com>
2022-07-15 21:50:04 +0700

    update page 1 & responsive

--- erodate/index.php
+++ erodate/index.php
@@ -3,10 +3,12 @@
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 $path = "pages/{$page}.php";
 
-require 'layout/header.php';
+require 'layout/TheHeader.html';
 
 if (file_exists($path)) {
     require "{$path}";
+} else {
+    require "./modules/404.php";
 }
 
-require 'layout/footer.php';
+require 'layout/TheFooter.html';
